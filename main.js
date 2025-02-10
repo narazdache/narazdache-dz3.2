e3e5eff0c30e0334d7848f164042b457c0364322
narazdache <nestortrotskii@gmail.com>
2025-02-10 18:42:02 +0300

    йоу

---
+++ main.js
@@ -0,0 +1,7 @@
+function getLines() {
+    let line1 = prompt("введіт перший рядок:");
+    let line2 = prompt("введіть другий рядок:");
+    let line3 = prompt("введіть третій рядок:");
+
+    console.log(`${line1} ${line2} ${line3}`);
+}
